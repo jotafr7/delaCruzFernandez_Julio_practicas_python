92a2f2da27907f3959dcc94105049c43e9e11527
jotafr7 <jotafr24@gmail.com>
2026-05-05 22:55:37 +0200

    Mis prácticas de Python

---
+++ actividad3.py
@@ -0,0 +1,5 @@
+nombre_alumno = input("¿Cual es tu nombre?")
+curso_alumno = input("¿En que curso estas?")
+grupo_alumno = input("¿En que grupo estas?")
+
+print(f"Hola {nombre_alumno} ¿como estas hoy? Veo que estas en el curso de {curso_alumno} y en el grupo {grupo_alumno}.")
---
+++ actividad4.py
@@ -0,0 +1,7 @@
+año_nacimiento = int(input("¿En que año naciste?"))
+
+anio_actual = 2026
+
+edad = anio_actual - año_nacimiento
+
+print(f"Tu edad es aproximadamente de {edad} años.")
