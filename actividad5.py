46cf2c8fcad06e9daaca111a93fd0334ab020d53
jotafr7 <jotafr24@gmail.com>
2026-05-07 10:37:56 +0200

    Actualización de actividad5.py

--- actividad5.py
+++ actividad5.py
@@ -1,6 +1,6 @@
 edad = int(input("Introduce tu edad: "))
 
-if edad >= 0 and edad <= 40:
+if edad >= 0 and edad <= 50:
     print("La edad introducida es válida")
 else:
     print("La edad introducida no es válida")
