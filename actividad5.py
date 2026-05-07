1df3db8d780d2b6d876944c2846c39899bd9d974
jotafr7 <jotafr24@gmail.com>
2026-05-07 12:20:19 +0200

    practicas python

--- actividad5.py
+++ actividad5.py
@@ -1,6 +1,6 @@
 edad = int(input("Introduce tu edad: "))
 
-if edad >= 0 and edad <= 50:
+if edad >= 16 and edad <= 40:
     print("La edad introducida es válida")
 else:
     print("La edad introducida no es válida")
