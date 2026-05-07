a06602b6df87ba5fe4ef612989df5ce689582b18
jotafr7 <jotafr24@gmail.com>
2026-05-07 12:30:50 +0200

    practicas_python

---
+++ actividad6.py
@@ -0,0 +1,9 @@
+numero = int(input("Introduce un número: "))
+
+if numero > 0:
+    print("El número introducido es positivo")
+
+elif numero == 0:
+    print("El número introducido es cero")
+else:
+    print("El número introducido es negativo")
