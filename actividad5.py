77f2334e75dc281467f82685ec7a6f44bb0cce62
jotafr7 <jotafr24@gmail.com>
2026-05-07 10:31:58 +0200

    practicas python

--- actividad5.py
+++ actividad5.py
@@ -1,1 +1,7 @@
-print("HGola, bienvenido a la práctica de Python")
+edad = int(input("Ingrese su edad: "))
+
+if edad > 16 and edad < 40:
+    print(f"La edad de {edad} años es valida para ser futbolista.")
+
+else:
+    print(f"La edad de {edad} años no es valida para ser futbolista.")
