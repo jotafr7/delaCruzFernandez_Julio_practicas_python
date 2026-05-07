829236f7ff5dd734bb2b7a29706453b2b94a4939
jotafr7 <jotafr24@gmail.com>
2026-05-07 12:57:11 +0200

    practicas_python

---
+++ actividad7.py
@@ -0,0 +1,19 @@
+print("EQUIPOS DE SEGUNDA DIVISION")
+print("1. Cordoba CF.")
+print("2. Granada CF.")
+print("3. Cadiz CF.")
+print("4. Albacete Balompie.")
+
+opcion =  input("Introduce el número del equipo que quieres ver:")
+
+if opcion == "1":
+    print("El Cordoba CF es la mejor opcion")
+
+elif opcion == "2":
+    print("El Granada CF es la mejor opcion")
+
+elif opcion == "3":
+    print("El Cadiz CF es la mejor opcion")
+
+else:
+    print("El Albacete Balompie es la mejor opcion")
---
+++ actividad8.py
@@ -0,0 +1,7 @@
+nota_alumno = int(input("¿Cual es la nota de tu ultimo examen?"))
+
+if nota_alumno >= 5:
+    print(f"Estas aprobado, ya que tu nota es un {nota_alumno}")
+
+else:
+    print(f"Estas suspenso porque tu nota es un {nota_alumno}")
