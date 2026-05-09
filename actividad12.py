71a96431900bf8da8aedaaa7a783c9ba718a94db
jotafr7 <jotafr24@gmail.com>
2026-05-09 08:51:06 +0200

    practicas_python

--- actividad12.py
+++ actividad12.py
@@ -1,4 +1,4 @@
-numero = int(input("Introduce el numeero que quieres multiplicar:"))
+numero = int(input("Dime que numero quieres multiplicar:"))
 
 for num in range (1, 11):
-    print(f"{numero} x {num} = {numero * num}")
+    print(f"Multiplicacion {numero} x {num} = {numero * num}")
---
+++ actividad13.py
@@ -0,0 +1,6 @@
+suma = 0
+
+for numero in range (1, 101):
+    suma = suma + numero
+
+print(f"La suma de todo en total es de: {suma}")
