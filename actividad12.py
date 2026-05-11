c91f75879310c8eef1875ae7bc6038818a7a63fb
jotafr7 <jotafr24@gmail.com>
2026-05-11 21:47:27 +0200

    practica_python

--- actividad12.py
+++ actividad12.py
@@ -1,6 +1,6 @@
 numero = int(input("Dime que numero quieres multiplicar:"))
 
 for num in range (1, 11):
-    print(f"Multiplicacion: {numero} x {num} = {numero * num}")
+    print(f"Multiplicamos: {numero} x {num} = {numero * num}")
 
     
